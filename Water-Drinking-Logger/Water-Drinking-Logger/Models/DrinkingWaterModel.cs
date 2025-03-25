9cedd7aca3fa78007241a0e72220c3204639ff78
Rody10 <rodneysimbatholanah@gmail.com>
2025-03-25 20:04:46 +0200

    implemented creation of a record

---
+++ Water-Drinking-Logger/Water-Drinking-Logger/Models/DrinkingWaterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Water_Drinking_Logger.Models
+{
+    public class DrinkingWaterModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Water-Drinking-Logger/Water-Drinking-Logger/Pages/Create.cshtml.cs
+++ Water-Drinking-Logger/Water-Drinking-Logger/Pages/Create.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Water_Drinking_Logger.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Water_Drinking_Logger.Pages
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        public CreateModel(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DrinkingWaterModel DrinkingWater { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                $"INSERT INTO drinking_water(date, quantity) VALUES('{DrinkingWater.Date}', {DrinkingWater.Quantity})";
+
+                tableCmd.ExecuteNonQuery();
+            }
+            return RedirectToPage("./Index");
+        }
+        
     }
 }
